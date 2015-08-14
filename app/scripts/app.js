9b9076919e042896a5e6ebb3a885c1613f598590
Grant Schulte <schulte.grant@gmail.com>
2015-08-13 23:15:51 -0400

    Adds more colors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,9 @@ seinfeldApp.value('appName', 'Seinfeld Quotes');
 seinfeldApp.value('backgroundColors', [
   '255,153,0',
   '50,153,187',
-  '188,188,188'
+  '188,188,188',
+  '179,204,87',
+  '239,116,111'
 ]);
 
 seinfeldApp.config(function ($routeProvider, $locationProvider) {
