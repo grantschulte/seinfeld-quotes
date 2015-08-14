aa6312ec80e63f147bb0543ed7ebc5521791943a
Grant Schulte <schulte.grant@gmail.com>
2015-08-13 22:01:14 -0400

    Edits start.js

--- db/start.js
+++ db/start.js
@@ -2,18 +2,18 @@
 
 var mongoose = require('mongoose');
 var env = process.env.NODE_ENV || 'development';
-var dbUrl;
+var dbUri;
 
 if (env === 'development') {
-  dbUrl = 'mongodb://localhost/seinfeld';
+  dbUri = 'mongodb://localhost/seinfeld';
 }
 else if (env === 'production') {
-  dbUrl = 'mongodb://heroku_vgnvcn5s:l1vhv313e9g4l8ggjls3m2j5oo@ds033143.mongolab.com:33143/heroku_vgnvcn5s';
+  dbUri = 'mongodb://heroku_vgnvcn5s:l1vhv313e9g4l8ggjls3m2j5oo@ds033143.mongolab.com:33143/heroku_vgnvcn5s';
 }
 
-mongoose.connect(dbUrl, function(err) {
+mongoose.connect(dbUri, function(err) {
   if(err) {
-    console.log('Connection Error', err);
+    console.log('Connection Error', err, dbUri);
   }
   else {
     console.log('Connection Successful');
